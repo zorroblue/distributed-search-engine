fb22a29637a8c445eb8b75db46684fc217b23208
Rameshwar Bhaskaran <rameshwar.zorro@gmail.com>
2018-03-31 17:46:00 +0530

    Added retries and set timeouts

--- master-replica.py
+++ master-replica.py
@@ -9,36 +9,42 @@ import grpc
 import search_pb2
 import search_pb2_grpc
 
+MAX_RETRIES = 3
+
+
 def build_parser():
 	parser = ArgumentParser()
 	parser.add_argument('--master',
 			dest='master', help='Master IP address',
 			default='localhost:50051',
 			required=False)
-	parser.add_argument('--replica',
-			dest='replica',
-			default='localhost:50051',
-			help='Replica IP address',
-			required=False)
 	return parser
 
 
 def run(server_ip):
+	retries = 0
 	while True:
 		time.sleep(1)
 		channel = grpc.insecure_channel(server_ip)
 		stub = search_pb2_grpc.HealthCheckStub(channel)
 		request = search_pb2.HealthCheckRequest(healthCheck = 'is_working?')
 		try :
-			response = stub.Check(request, timeout = 1)
+			print("Sending heartbeat message to master")
+			response = stub.Check(request, timeout = 10)
 			print(response)
+			# reset retries
+			retries = 0
 		except Exception as e:
 			if str(e.code()) == "StatusCode.DEADLINE_EXCEEDED":
-				print("DEADLINE_EXCEEDED!")
+				print("DEADLINE_EXCEEDED!\n")
 			if str(e.code()) == "StatusCode.UNAVAILABLE":
-				print("UNAVAILABLE!")
-			master.serve()
-			break;
+				print("UNAVAILABLE!\n")
+			retries += 1
+			if retries > MAX_RETRIES:
+				master.serve('replica')
+				break;
+			else:
+				print("Retrying again #" + str(retries))
 
 
 def main():
--- master.py
+++ master.py
@@ -13,28 +13,28 @@ _ONE_DAY_IN_SECONDS = 60 * 60 * 24
 
 class Master(search_pb2_grpc.SearchServicer):
 
-	def __init__(self):
-		# TODO : Add database
-		self.db = None
+	def __init__(self, db_name):
+		
+		self.db = db_name
 		self._HEALTH_CHECK_TIME = 0
 
 	def SearchForString(self, request, context):
 		search_term = request.query
-		urls = querydb('master', search_term)
-		print "received query: ",search_term
+		urls = querydb(self.db, search_term)
+		print("Received query: " + search_term+"\n")
 		return search_pb2.SearchResponse(urls=urls)
 
 	def Check(self, request, context):
 		print request
-		# if not self._HEALTH_CHECK_TIME%4:
-		# 	time.sleep(2)
 		self._HEALTH_CHECK_TIME += 1
-		return search_pb2.HealthCheckResponse(status = "working_:)")
+		return search_pb2.HealthCheckResponse(status = "STATUS: Master server up!")
 
-def serve():
+
+def serve(db_name):
 	server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
-	search_pb2_grpc.add_SearchServicer_to_server(Master(), server)
-	search_pb2_grpc.add_HealthCheckServicer_to_server(Master(), server)
+	# TODO: Check if the first param for both the below should be the same object
+	search_pb2_grpc.add_SearchServicer_to_server(Master(db_name), server)
+	search_pb2_grpc.add_HealthCheckServicer_to_server(Master(db_name), server)
 	server.add_insecure_port('[::]:50051')
 	server.start()
 	print "Starting master"
@@ -46,4 +46,4 @@ def serve():
 
 
 if __name__ == '__main__':
-	serve()
+	serve('master')
