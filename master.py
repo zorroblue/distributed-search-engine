5af492fb094da7fafe04f11413a12f80753464ee
Rameshwar Bhaskaran <rameshwar.zorro@gmail.com>
2018-04-01 16:52:53 +0530

    Logging works for master

--- master.py
+++ master.py
@@ -2,49 +2,76 @@ from concurrent import futures
 import time
 import math
 
+from argparse import ArgumentParser
+import argparse
+
 import grpc
 import search_pb2
 import search_pb2_grpc
 
-from utils import querydb
+import logging
+
+from utils import querydb, init_logger, parse_level
 
 _ONE_DAY_IN_SECONDS = 60 * 60 * 24
 
 
+def build_parser():
+	parser = ArgumentParser()
+	choices = ['DEBUG', 'INFO', 'WARNING', 'ERROR', 'CRITICAL']
+	parser.add_argument('--logging',
+			dest='logging_level', help='Logging level',
+			choices=choices,
+			default='DEBUG',
+			required=False)
+	return parser
+
+
 class Master(search_pb2_grpc.SearchServicer):
 
-	def __init__(self, db_name):
-		
+	def __init__(self, db_name, logging_level=logging.DEBUG):
 		self.db = db_name
 		self._HEALTH_CHECK_TIME = 0
-
+		self.logger = init_logger(db_name, logging_level)
+	
 	def SearchForString(self, request, context):
 		search_term = request.query
 		urls = querydb(self.db, search_term)
-		print("Received query: " + search_term+"\n")
+		self.logger.debug("Received query: " + search_term)
 		print urls
 		return search_pb2.SearchResponse(urls=urls)
 
 	def Check(self, request, context):
-		print request
+		self.logger.debug("Received heartbeat query from backup")
 		self._HEALTH_CHECK_TIME += 1
 		return search_pb2.HealthCheckResponse(status = "STATUS: Master server up!")
 
 
-def serve(db_name):
+def serve(db_name, logging_level=logging.DEBUG):
 	server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
-	# TODO: Check if the first param for both the below should be the same object
-	search_pb2_grpc.add_SearchServicer_to_server(Master(db_name), server)
-	search_pb2_grpc.add_HealthCheckServicer_to_server(Master(db_name), server)
+	master = Master(db_name, logging_level)
+	search_pb2_grpc.add_SearchServicer_to_server(master, server)
+	search_pb2_grpc.add_HealthCheckServicer_to_server(master, server)
 	server.add_insecure_port('[::]:50051')
 	server.start()
+	master.logger.info("Starting server")
 	print "Starting master"
 	try:
 		while True:
 			time.sleep(_ONE_DAY_IN_SECONDS)
 	except KeyboardInterrupt:
+		master.logger.info("Shutting down server")
+		logging.shutdown()
 		server.stop(0)
 
 
+def main():
+	parser = build_parser()
+	options = parser.parse_args()
+	level = options.logging_level
+	logging_level = parse_level(level)
+	serve('master', logging_level)
+
+
 if __name__ == '__main__':
-	serve('master')
+	main()
--- utils.py
+++ utils.py
@@ -5,6 +5,11 @@ Utility functions
 import pprint
 import json
 from pymongo import MongoClient
+import logging
+
+import argparse
+from argparse import ArgumentParser
+
 
 def querydb(sender, search_term):
 	'''Query on mongodb database for suitable response for search term
@@ -21,3 +26,32 @@ def querydb(sender, search_term):
 	if response is not None:
 		return response["urls"]
 	return []
+
+
+def init_logger(db_name, logging_level):
+	logger = logging.getLogger(db_name)
+	logger.setLevel(logging_level)
+	fh = logging.FileHandler('log'+db_name+'.log')
+	fh.setLevel(logging_level)
+	# create a logging format
+	formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+	fh.setFormatter(formatter)
+	logger.addHandler(fh)
+	return logger
+
+
+def parse_level(level):
+	if level == 'DEBUG':
+		logging_level = logging.DEBUG
+	elif level == 'INFO':
+		logging_level = logging.INFO
+	elif level == 'WARNING':
+		logging_level = logging.WARNING
+	elif level == 'ERROR':
+		logging_level = logging.ERROR
+	elif level == 'CRITICAL':
+		logging_level = logging.CRITICAL
+	else:
+		message = 'Invalid choice! Please choose from DEBUG, INFO, WARNING, ERROR, CRITICAL'
+		argparse.ArgumentError(self, message)
+	return logging_level
